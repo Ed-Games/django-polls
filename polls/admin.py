31d60d61734a8fd1a86a76a209a8dbdc5f73ac15
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-21 14:40:56 -0300

    created: project built

---
+++ polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+# Register your models here.
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Text', {'fields' : ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInLine]
+
+    list_display = ('question_text', 'pub_date', 'wasPublishedRecently')
+
+    list_filter = ['pub_date']
+    
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
---
+++ polls/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def wasPublishedRecently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    wasPublishedRecently.admin_order_field = 'pub_date'
+    wasPublishedRecently.boolean = True
+    wasPublishedRecently.short_description = 'Published recently?'
+    
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+    def __str__(self):
+        return self.question_text
+    
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.choice_text
+    
